978d3899db7cb135929fcb409a472d4a624b2943
shahnawaj-poold <shahnawaj@poold.co>
2025-11-09 08:00:54 +0100

    new docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  server: {
+    host: true,
+    port: 3000
+  }
 })
